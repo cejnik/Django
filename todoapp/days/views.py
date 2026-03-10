7bc4a0d63db3898dac4e45120a34c2a87a93c319
cejnik <chejnma@gmail.com>
2026-03-10 18:01:39 +0100

    redirect from number to days

--- todoapp/days/views.py
+++ todoapp/days/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+list_of_days = {
+    'monday': 'Jít nakoupit',
+    'tuesday': 'učit se Python',
+    'wednesday': 'Vynést koš',
+    'thursday': 'Dojít nakoupit',
+    'friday': 'Naprogramovat hru',
+    'saturday': 'Zajít do kina',
+    'sunday': 'Jít cvičit'
+}
 
 def daynumber(request, dayinweek_number):
-    return HttpResponse(f'Den: {dayinweek_number}')
-
+    #vytvoření listu z keys z list_of_days
+    days_names =list(list_of_days.keys())
+    if dayinweek_number > len(days_names):
+        return HttpResponseNotFound('Zadali jste špatné číslo dne.')
+    
+    current_day = days_names[dayinweek_number-1]
+    return HttpResponseRedirect(current_day)
+    
 
 def daytext(request, dayinweek_string):
     day_description = ''
