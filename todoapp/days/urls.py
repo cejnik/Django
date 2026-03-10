4c25d495c67f9031d2ad21b9594e4941fc9ff205
cejnik <chejnma@gmail.com>
2026-03-10 18:49:15 +0100

    redirection vs reverse, default values in main page

--- todoapp/days/urls.py
+++ todoapp/days/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('<int:dayinweek_number>', views.daynumber),
-    path('<str:dayinweek_string>', views.daytext),
+    path('<str:dayinweek_string>', views.daytext, name='days_tasks'),
 
 ]
--- todoapp/days/views.py
+++ todoapp/days/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import redirect
 
 list_of_days = {
     'monday': 'Jít nakoupit',
@@ -11,34 +13,40 @@ list_of_days = {
     'sunday': 'Jít cvičit'
 }
 
+def index(request):
+    content = ''
+    days = list(list_of_days.keys())
+
+    #HTML content
+    content = '<ul>'
+    for one_day in days:
+        url = reverse('days_tasks', args=[one_day]) #reverse vrací URL, redirect vrací HTTP objekt!!
+        content += f'<li><a href="{url}">{one_day.capitalize()}</a></li>'
+    content += '</ul>'
+      #//HTML content
+      
+    return HttpResponse(content)
+  
+
+
 def daynumber(request, dayinweek_number):
     #vytvoření listu z keys z list_of_days
     days_names =list(list_of_days.keys())
     if dayinweek_number > len(days_names):
         return HttpResponseNotFound('Zadali jste špatné číslo dne.')
     
-    current_day = days_names[dayinweek_number-1]
-    return HttpResponseRedirect(current_day)
-    
+    redirect_day = days_names[dayinweek_number-1]
+    # redirect_path = reverse('days_tasks', args=[redirect_day])
+    # return HttpResponseRedirect(redirect_path)
+    return redirect('days_tasks', redirect_day)  # vrací na základě importu reverse jako shortscut, vrací HTTPresponse objekt, nikoliv URL
 
 def daytext(request, dayinweek_string):
-    day_description = ''
-    if dayinweek_string == 'monday':
-        day_description = 'Pondělí'
-    elif dayinweek_string == 'tuesday':
-        day_description = 'úterý'
-    elif dayinweek_string == 'wednesday':
-        day_description = 'Středa'
-    elif dayinweek_string == 'thursday':
-        day_description = 'Čtvrtek'
-    elif dayinweek_string == 'friday':
-        day_description = 'Pátek'
-    elif dayinweek_string == 'saturday':
-        day_description = 'sobota'
-    elif dayinweek_string == 'sunday':
-        day_description = 'neděle'
-    else:
-        return HttpResponseNotFound("Neexistují den")
+    try:
+        task = list_of_days[dayinweek_string]
+        formated_task = f'<h1>{task}</h1>'
+        return HttpResponse(formated_task)
+    except:
+        return HttpResponseNotFound('Špatně zadaný údaj')
 
 
-    return HttpResponse(f'Jaký je den: {day_description}')
+
