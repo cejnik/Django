e7a63ef9af28f3bf6528fc93174ef5549f81c437
cejnik <chejnma@gmail.com>
2026-03-13 12:36:06 +0100

    refactor: OpenCode test

--- greet/greet_app/urls.py
+++ greet/greet_app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<int:greet_number>', views.greet_number),
     path('<str:greet_string>', views.greet_string, name='greeting_string'),
-
+    path('<int:greet_number>', views.greet_number),
 ]
--- greet/greet_app/views.py
+++ greet/greet_app/views.py
@@ -1,31 +1,33 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound 
 from django.urls import reverse
+
+greet_list = {
+    'ahoj': 'nic',
+    'cau': 'tak zase nic',
+    'dobrý den': 'nic....',
+}
 
 
 def index(request):
-    content = ''
-    greeting_list = list(greet_list.keys())
-    content +='<ul>'
-    for one_greet in greeting_list:
+    content = '<ul>'
+    for one_greet in greet_list.keys():
         url = reverse('greeting_string', args=[one_greet])
-        content += f'<li><a href="{url}">{one_greet}</li>'
+        content += f'<li><a href="{url}">{one_greet}</a></li>'
     content += '</ul>'
     return HttpResponse(content)
 
-greet_list = {
-    'ahoj':'nic',
-    'cau':'tak zase nic',
-    'dobrý den':'nic....'
-    }
 
 def greet_string(request, greet_string):
-    greet_name = greet_list[greet_string]
+    greet_name = greet_list.get(greet_string)
+    if greet_name is None:
+        return HttpResponseNotFound("Pozdrav nenalezen")
     return HttpResponse(greet_name)
+
 
 def greet_number(request, greet_number):
     greeting_list = list(greet_list.keys())
-    if greet_number > len(greeting_list):
+    if greet_number > len(greeting_list) or greet_number < 1:
         return HttpResponseNotFound("Pozdrav nenalezen")
-    current_greet = greeting_list[greet_number-1]
+    current_greet = greeting_list[greet_number - 1]
     return redirect('greeting_string', current_greet)
