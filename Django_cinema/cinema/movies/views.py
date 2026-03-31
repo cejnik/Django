a43b7f16745516b2956df0f951faf4f41ac59652
cejnik <chejnma@gmail.com>
2026-03-31 19:27:12 +0200

    add new logic to control reservation vs hall capacity, add new messages errors

--- Django_cinema/cinema/movies/views.py
+++ Django_cinema/cinema/movies/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from . models import Film, Screening, Reservation
 from . forms import RegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.db.models import Sum
 
 # Create your views here.
 def index(request):
@@ -41,12 +43,25 @@ def reservation(request):
 @login_required
 def create_reservation(request, screening_id):
     screening_now = get_object_or_404(Screening, id = screening_id)
-
     try:
         tickets_count = int(request.POST.get('tickets_count'))
         if tickets_count <= 0:
+            messages.error(request, "Please enter a valid ticket count.")
             return redirect('movie_detail', slug=screening_now.film.slug)
         if tickets_count > 0:
+                if screening_now.hall is None:
+                    messages.error(request, 'This screening does not have an assigned hall.')
+                    return redirect('movie_detail', slug=screening_now.film.slug)
+
+                existing_reservation = Reservation.objects.filter(user=request.user, screening=screening_now).first()
+                current_user_old_tickets = existing_reservation.tickets_count if existing_reservation else 0
+                reserved_tickets_data = Reservation.objects.filter(screening=screening_now).aggregate(Sum('tickets_count'))
+                reserved_tickets = reserved_tickets_data['tickets_count__sum'] or 0
+                new_total_tickets = reserved_tickets - current_user_old_tickets + tickets_count
+                if new_total_tickets > screening_now.hall.capacity:
+                    messages.error(request, 'There are not enough seats available for this screening.')
+                    return redirect('movie_detail', slug=screening_now.film.slug)
+
                 reservation, created = Reservation.objects.get_or_create(
                                         user = request.user,
                                         screening = screening_now,
@@ -56,7 +71,9 @@ def create_reservation(request, screening_id):
                 reservation.save()
                 
     except (TypeError, ValueError):
+        messages.error(request, "Please enter a valid ticket count.")
         return redirect('movie_detail', slug=screening_now.film.slug)
+    messages.success(request, 'Your reservation has been saved.')
     return redirect('reservation')
     
 @login_required      
@@ -64,6 +81,7 @@ def delete_reservation(request, reservation_id):
     if request.method == 'POST':
         reservation = get_object_or_404(Reservation, user=request.user, id = reservation_id )
         reservation.delete()
+        messages.error(request, "Your reservation has been deleted.")
         return redirect('reservation')
     else:
         return redirect('reservation')
