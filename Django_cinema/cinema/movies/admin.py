6334f03ee478398e69c1f30365c06597cdaaea5f
cejnik <chejnma@gmail.com>
2026-04-04 10:37:44 +0200

    add admin options

--- Django_cinema/cinema/movies/admin.py
+++ Django_cinema/cinema/movies/admin.py
@@ -8,5 +8,19 @@ admin.site.register(Hall)
 
 @admin.register(Seat)
 class SeatAdmin(admin.ModelAdmin):
-    list_display=['hall', 'row', 'number']
+    list_display = ['hall', 'row', 'number']
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    @admin.display(description='Movie')
+    def film(self, obj):
+        return obj.screening.film
+    def hall(self, obj):
+        return obj.screening.hall
+    def screening_time(self, obj):
+        return obj.screening.screening_time
+
+
+    list_display = ['user', 'tickets_count', 'film', 'hall', 'screening_time']
+
 
